0cd355fe7df196d0eeaf4b790cec654a93baf060
Mischa Deriu <tobitashinchi@protonmail.com>
2023-12-28 16:37:49 +0100

    Initial commit

---
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -0,0 +1,12 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+
+        // Game Grid
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
+    }
+}
